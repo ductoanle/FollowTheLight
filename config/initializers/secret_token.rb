c636c0414c6ad07c212b6cff0d5c103b36896ce9
ethan.le <ethan.le@ufinity.com>
2011-01-19 23:34:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FollowTheLight::Application.config.secret_token = '1e6702cbfeb68a4714f02ae437c8e78207a5b57b9ef1b606470baae938468b6c04387d8c6f842319048eb3f6a1263d40dee749ecc0e3113c44ac42564ab918bb'
