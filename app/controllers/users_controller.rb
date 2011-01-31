1a7b9d6822848d7e613b542b6372f82ac283eae4
ethan.le <ethan.le@ufinity.com>
2011-01-31 17:41:06 +0800

    Finishing the users signup task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
+    @title = "Sign Up"
   end
 
   def index
@@ -9,6 +10,18 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #Handle a successful save
+      flash[:success] = "Welcome to the Sample Apps!"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,17 +3,63 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
+  describe "GET 'signup'" do
+    describe "Render page" do
+      it "should be successful" do
+        get :new
+        response.should be_success
+      end
+      it "should have proper title" do
+        get :new
+        response.should have_selector("title", :content=>"Sign Up")
+      end
+    end
+    describe "failed submit new user" do
+      before(:each) do
+        @attr = {:name => '', :email => '', :password => '', :password_confirmation => ''}
+      end
+
+      it 'should not create a user' do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it 'should have the right title' do
+        post :create, :user=>@attr
+        response.should have_selector("title", :content=>'Sign Up')
+      end
+
+      it 'should render the new page ' do
+        post :create, :user=>@attr
+        response.should render_template('new')
+      end
     end
 
-    it "should have proper title" do
-      get 'new'
-      response.should have_selector("title", :content=>"Sign Up")
+    describe "successful submit a new user" do
+      before(:each) do
+        @attr = {:name => 'Ethan Le', :email =>'ethan.le@ufinity.com', :password => 'ductoanle',
+                :password_confirmation => 'ductoanle'}
+      end
+
+      it 'should create a new user' do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User,:count).by(1)
+      end
+
+      it 'should redirect to user show page' do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it 'should have a welcome message' do
+        post :create, :user=>@attr
+        flash[:success].should =~ /Welcome to the sample app/i
+      end
     end
   end
+
 
   describe "GET 'show'" do
     before (:each) do
@@ -27,11 +73,11 @@ describe UsersController do
 
     it "should have pull the correct user " do
       get :show, :id => @user
-      assign(:user).should == @user
+      assigns(:user).should == @user
     end
 
     it "should have the username" do
-      get :show, id => @user
+      get :show, :id => @user
       response.should have_selector("h1", :content => @user.name)
     end
 
@@ -40,5 +86,4 @@ describe UsersController do
       response.should have_selector("h1>img", :class =>"gravatar")
     end
   end
-
 end
